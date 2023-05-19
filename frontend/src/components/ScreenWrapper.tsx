994bce699d170ceb3183ac4426f88972aed05209
shobhitsundriyal <shobhitsundriyal0170@gmail.com>
2023-05-19 12:27:21 +0530

    made custom button classes, restructured home page(no visual changes)

--- frontend/src/components/ScreenWrapper.tsx
+++ frontend/src/components/ScreenWrapper.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const ScreenWrapper = ({children}:{children:any}) => {
     return (
         <main
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,20 +1,37 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import Link from 'next/link'
 import ScreenWrapper from '@/components/ScreenWrapper'
+import { useRouter } from 'next/router'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const router = useRouter()
+  const connectWallet = async () => {}
+
+  const handleSignIn = async (userType: "agency" | "user") => {
+    try {
+      await connectWallet()
+
+      if(userType === "agency") {
+        router.push("/agency/signin")
+      } else {
+        router.push("/user/signin")
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   return (
     <ScreenWrapper>
       <div className='w-3/12 flex justify-between'>
-        <span className='flex items-center justify-center py-2 px-4 w-40 text-lg rounded-[26px] bg-slate-800 text-white hover:text-slate-800 hover:bg-white hover:border-[1px] hover:border-slate-800'>
-          <Link href={'/agency/signin'}>Agency Login</Link>
-        </span>
-        <span className='flex items-center justify-center py-2 px-4 w-40 text-lg rounded-[26px] border-[1px] border-slate-800 bg-[#E9E8E4] hover:bg-slate-800 hover:text-white'>
-          <Link href={'/user/signin'}>User Login</Link>
-        </span>  
+        <button className=' btn-primary-color-changing py-2 px-4 w-40 text-lg rounded-[26px]' onClick={() => handleSignIn("agency")}>
+          Agency Login
+        </button>
+
+        <button className='btn-secondary-color-changing py-2 px-4 w-40 text-lg  rounded-[26px]' onClick={() => handleSignIn("user")}>
+          User Login
+        </button>  
       </div>
     </ScreenWrapper>
   )
