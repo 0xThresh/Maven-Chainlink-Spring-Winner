f8d3073c7ed370ae401a54864fba8330d352caef
Shobhit Sundriyal <shobhitsundriyal0170@gmail.com>
2023-06-03 15:30:39 +0530

    siderbar added(design) (#29)
    * siderbar added(design)
    
    * Svg renamed
    
    * removed sidebar margin

--- frontend/src/pages/agency/[name].tsx
+++ frontend/src/pages/agency/[name].tsx
@@ -1,12 +1,17 @@
+import DashboardSideBar from '@/components/DashboardSideBar'
 import DashboardNavBar from '@/components/DashboardNavBar'
 import React from 'react'
 
 const AgencyDashboard = () => {
   return (
-    <div>
+    <>
       <DashboardNavBar />
-      AgencyDashboard
-    </div>
+      {/** header can go here */}
+      <div className="flex">
+        <DashboardSideBar />
+        AgencyDashboard
+      </div>
+    </>
   )
 }
 
