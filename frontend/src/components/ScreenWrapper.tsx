a718cb4d3d0bbc19d87d45cad68937c80382ab44
Shobhit Sundriyal <shobhitsundriyal0170@gmail.com>
2023-06-05 16:23:35 +0530

    made custom button classes, restructured home page(no visual changes) (#21)
    * made custom button classes, restructured home page(no visual changes)
    
    * removed handle signin function from homepage, added little animation

--- frontend/src/components/ScreenWrapper.tsx
+++ frontend/src/components/ScreenWrapper.tsx
@@ -1,14 +1,12 @@
-import React from 'react'
-
 const ScreenWrapper = ({children}:{children:any}) => {
-    return (
-        <main
-          className='bg-bgColor h-[100vh] w-full flex flex-col items-center justify-center'
-        >
-          <img src="/assets/logo.svg" alt="" className='h-40 w-40' />
-          {children}
-        </main>
-      )
-}
+  return (
+    <main
+      className="bg-bgColor h-[100vh] w-full flex flex-col items-center justify-center"
+    >
+      <img src="/assets/logo.svg" alt="" className="h-40 w-40" />
+      {children}
+    </main>
+  );
+};
 
-export default ScreenWrapper
+export default ScreenWrapper;
--- frontend/src/pages/agency/signin.tsx
+++ frontend/src/pages/agency/signin.tsx
@@ -1,36 +1,40 @@
-import ScreenWrapper from '@/components/ScreenWrapper'
-import Router from 'next/router'
-import React, { FormEvent } from 'react'
+import ScreenWrapper from "@/components/ScreenWrapper";
+import Router from "next/router";
+import { FormEvent } from "react";
 
 const Signin = () => {
 
   const signin = (e:FormEvent) => {
-    e.preventDefault()
-    Router.push('/agency/:name')
-  }
+    e.preventDefault();
+    Router.push("/agency/:name");
+  };
 
   return (
     <ScreenWrapper>
-      <form onSubmit={signin} className='flex flex-col items-center'>
-        <img src="/assets/imageplaceholder.svg" alt="placeholder image" className='h-24 mb-2' />
-        <label htmlFor="profile" className='w-[100px] flex items-center justify-center bg-slate-800 text-white py-1 rounded-md text-sm'>Upload</label>
+      <form onSubmit={signin} className="flex flex-col items-center animate-height-agency-form overflow-hidden">
+        <img src="/assets/imageplaceholder.svg" alt="placeholder image" className="h-24 mb-2" />
+
+        <label htmlFor="profile" className="w-[100px] btn-primary py-1 rounded-md text-sm cursor-pointer">Upload</label>
         <input type="file" id="profile" hidden />
-        <fieldset className='mt-6'>
-          <legend className='ml-2 translate-y-2'>Agency Name</legend>
-          <input type="text" className='w-96 h-10 outline-none border-[1px] border-black pl-2' />
+
+        <fieldset className="mt-6">
+          <legend className="ml-3 translate-y-2 bg-bgColor px-1">Agency Name</legend>
+          <input type="text" className="w-96 h-10 outline-none border-[1px] border-black pl-2 bg-bgColor  " />
         </fieldset>
-        <fieldset className='mt-6'>
-          <legend className='ml-2 translate-y-2'>Agency Name</legend>
-          <select className='w-96 h-10 outline-none border-[1px] border-black pl-2'>
+
+        <fieldset className="mt-6">
+          <legend className="ml-3 translate-y-2 bg-bgColor px-1">Primary Language</legend>
+          <select className="w-96 h-10 outline-none border-[1px] border-black pl-2 bg-bgColor">
             <option value="French">FRENCH</option>
             <option value="English" selected>ENGLISH</option>
             <option value="Spanish">SPANISH</option>
           </select>
         </fieldset>
-        <button type="submit" className='h-10 w-96 mt-8 flex items-center justify-center bg-slate-800 text-white'>Submit</button>
+        
+        <button type="submit" className="btn-primary h-10 w-96 mt-8">Submit</button>
       </form>
     </ScreenWrapper>
-  )
-}
+  );
+};
 
-export default Signin
+export default Signin;
