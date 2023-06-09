a6b53a6923e762e6d75084a6dc1d9c386e213877
Merdi-kim <merdikimuha1@gmail.com>
2023-06-09 06:58:55 +0300

    add artifacts path

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -3,6 +3,9 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  paths: {
+    artifacts:'../frontend/artifacts'
+  }
 };
 
 export default config;
